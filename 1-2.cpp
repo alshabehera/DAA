2bf8354180095aefa719418e2f64359cf76bebe6
alsha <alshabehera7@gmail.com>
2022-09-28 16:47:33 +0530

    first

---
+++ 1-2.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int floorsqrt(int x)
+{
+
+    if (x==0||x==1)
+        return x;
+        int i=1,result=1;
+        while(result<=x)
+    {
+        i++;
+        result=i*i;
+    }
+    return i-1;
+}
+int main()
+{
+    int x;
+    cout<<"root square of 2 is "<<floorsqrt(2)<<endl;
+    cout<<"root square of 4 is "<<floorsqrt(4)<<endl;
+    cout<<"root square of 6 is "<<floorsqrt(6)<<endl;
+    return 0;
+
+}
