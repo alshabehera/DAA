aee0984b526a0361adc1926d034bd1f95d3f2345
ALSHA BEHERA <alshabehera7@gmail.com>
2023-07-29 12:34:55 +0530

    circular linked list

--- linkedList.cpp
+++ linkedList.cpp
@@ -4,7 +4,7 @@ using namespace std;
 struct node{
     int data;
     struct node *next;
-}*first=NULL;
+}*first=NULL,*second=NULL,*third=NULL;
 
 void create(int *A,int n){
     first=new node;
@@ -21,6 +21,24 @@ void create(int *A,int n){
         last->next=t;
         last=t;
     }
+}
+void create2(int *B,int n){
+    struct node *t,*last;
+    int i;
+    second=new node;
+    second->data= B[0];
+    second->next=NULL;
+    last=second;
+    for(i=1;i<n;i++){
+     t=new node;
+     t->data=B[i];
+     t->next=NULL;
+     last->next=t;
+     last=t;
+
+    }
+
+
 }
 
 int sum(struct node *first){
@@ -84,12 +102,12 @@ q->next=t;}
 
 }
 }
-void display(){
+void display(struct node* p){
     
-    while(first!=0){
-        cout<<first->data<<" ";
+    while(p!=0){
+        cout<<p->data<<" ";
        
-        first=first->next;
+       p=p->next;
     }
      
 }
@@ -156,15 +174,123 @@ while(p!=NULL){
 first=q;
 
 }
+void merge(struct node*f,struct node*s){
+    struct node *last;
+    third= new node;
+    if(f->data<s->data){
+        third=f;
+        last=f;
+        f=f->next;
+        third->next=NULL;
+        
+    }
+    else{
+        third=s;
+        last=s;
+        s=s->next;
+        third->next=NULL;
+    }
 
-int main(){
+    while(f!=NULL && s!=NULL){
+        if(f->data<s->data){
+            last->next=f;
+            last=f;
+            
+            f=f->next;
+            last->next=NULL;
+        }
+        else{  last->next=s;
+            last=s;
+            s=s->next;
+            last->next=NULL;
+           
+        }
+    }
+    if(f){
+        last->next=f;
+    }
+     if(s){
+        last->next=s;
+    }
+}
+int isLoop(struct node *first){
+    struct node *p;
+    struct node *q;
+    p=q=first;
+    do{
+        p=p->next;
+        q=q->next;
+        q=q!=0?q->next:q;
+    }
+    while(p && q && p!=q);
+    if(p==q){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+int circularDelete(struct node *p,int pos){
+struct node *q=NULL;
+int x;
+p=first;
+if(pos==1){
+    while(p->next!=first){
+        p=p->next;
+    }
+    if(p=first){
+        x=first->data;
+        delete first;
+        first=NULL;
+    }
+    else{
+        p->next=first->next;
+        x=first->data;
+        delete first;
+        first=p->next;
+
+    }}
+else{
+    for(int i=0;i<pos-2;i++){
+p=p->next;
+    }
+    q=p->next;
+    p->next=q->next;
+    x=q->data;
+    delete q;
+}  
+return x;
+
+}  
+
+
+
+
+int main()
+{
+    struct node *t1;
+    struct node *t2;
     
-    int A[]={3,4,5,23,56,87,89};
+    int A[]={3,6,10,23,56,87,89};
+    //int B[]={4,5,8,45,46,57,100};
     create(A,7);
+    display(first);
+    //create a loop
+    t1=first->next->next->next;
+    t2=first->next->next->next->next->next->next;
+    t2->next=t1;
+    
+    cout<<endl;
+    //cout<<isLoop(first);
+cout<<circularDelete(first,4);
+display(first);
+    //create2(B,7);
     //sortInsert(first,15);
     //Delete(first,4);
-    reverseLink(first);
-    display();
+    //reverseLink(first);
+    //merge(first,second);
+    //display(third);
+
     
     
     //insert(first,3,89);
