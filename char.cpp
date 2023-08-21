7ac0a1db22653a7b123c3112e266aadb1034d506
ALSHA BEHERA <alshabehera7@gmail.com>
2023-08-21 15:00:44 +0530

    char

--- char.cpp
+++ char.cpp
@@ -1,17 +1,36 @@
-#include <iostream>
+#include<iostream>
 
 using namespace std;
+int getLength(char *n){
+    int i=0;int count =0;
+    while(n[i]!='\0'){
+count++;
+i++;
+    }
+    return count;
+}
+void reverse(char *n){
+    int s=0;int e=getLength(n)-1;
+    while (s<e){
+        char temp;
+        temp=n[s];
+        n[s]= n[e];
+        n[e]=temp;
+        s++;e--;
 
-int main()
-{
-   char name[]="ALSHA";
-   int i;
-   for(i=0;name[i]!='\0';i++){
-       name[i]=name[i]+32;
-       
-   }
-   cout<<name;
-   
 
-    return 0;
+    }
+    cout<<n;
+
+
+}
+int main(){
+    char name[10];
+    cout<<"enter:";
+    cin>>name;
+    
+    int x=getLength(name);
+    cout<<"length: "<<x;
+   reverse(name);
+
 }
